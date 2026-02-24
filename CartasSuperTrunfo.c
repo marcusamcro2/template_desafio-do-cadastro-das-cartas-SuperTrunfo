9193a922cf3454d467277fdc09e8c3728e96c8a7
Marcus Vinicius Pontes da Silva <154470239+marcusamcro2@users.noreply.github.com>
2026-02-24 17:49:58 +0000

    Correção de erros do exercício intermediário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,20 +9,17 @@ int main() {
     char estado1, codigoe1[20], cidade1[20];  
     int habitantes1, pontos1; 
     float area1, PIB1, densidade1, perCapita1;
-    densidade1 = habitantes1/area1; // Cálculo da densidade demográfica
-    perCapita1 = PIB1/habitantes1; // Cálculo do PIB per capita
+    densidade1 = habitantes1/area1;
 
     // Carta 2
     char estado2, codigoe2[20], cidade2[20];  
     int habitantes2, pontos2; 
     float area2, PIB2, densidade2, perCapita2;
-    densidade2 = habitantes2/area2; // Cálculo da densidade demográfica
-    perCapita2 = PIB2/habitantes2; // Cálculo do PIB per capita
-
     
     /*O sistema vai gerar um pedido pelo "printf" ao usuário para inserir os dados das cartas,
     o "scanf", por sua vez, lê os dados e armazena nas respectivas variáveis*/
 
+    // Carta 1
     printf("Digite o estado da 1ª carta: ");
     scanf(" %c", &estado1);
 
@@ -44,7 +41,7 @@ int main() {
     printf("Digite os pontos turísticos da 1ª carta: ");
     scanf("%d", &pontos1);
 
-
+    // Carta 2
     printf("Digite o estado da 2ª carta: ");
     scanf(" %c", &estado2);
 
@@ -76,8 +73,8 @@ int main() {
     printf("Número de habitantes: %d\n", habitantes1);
     printf("Área: %.2fkm²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", PIB1);
-    printf("Densidade demográfica: %.2f habitantes/km²\n", densidade1);
-    printf("PIB per capita: %.2f reais\n", perCapita1);
+    printf("Densidade demográfica: %.2f habitantes/km²\n", densidade1 = habitantes1/area1); // Cálculo da densidade demográfica
+    printf("PIB per capita: %.2f reais\n", perCapita1 = PIB1/habitantes1); // Cálculo do PIB per capita
     printf("Número de pontos turísticos: %d\n", pontos1);
     
     printf("\nDados da 2ª carta:\n");
@@ -87,8 +84,8 @@ int main() {
     printf("Número de habitantes: %d\n", habitantes2);
     printf("Área: %.2fkm²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", PIB2);
-    printf("Densidade demográfica: %.2f habitantes/km²\n", densidade2);
-    printf("PIB per capita: %.2f reais\n", perCapita2);
+    printf("Densidade demográfica: %.2f habitantes/km²\n", densidade2 = habitantes2/area2); // Cálculo da densidade demográfica
+    printf("PIB per capita: %.2f reais\n", perCapita2 = PIB2/habitantes2); // Cálculo do PIB per capita
     printf("Número de pontos turísticos: %d\n", pontos2);
 
     printf("\nFim de jogo!\n"); // Mensagem de encerramento do jogo
